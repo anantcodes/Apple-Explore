50e0e16d20da028b49330fd623796773979d9779
Anant Kanchan <anant4265@gmail.com>
2021-01-07 21:14:28 +0530

    initial iOS commit

---
+++ Apple_Explore/Apple_ExploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_ExploreApp.swift
+//  Apple_Explore
+//
+//  Created by Anant Kanchan on 07/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Apple_ExploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple_Explore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apple_Explore
+//
+//  Created by Anant Kanchan on 07/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
